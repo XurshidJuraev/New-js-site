11de5b4da39e83b5c2faf85e9e9a378d65140413
XurshidJuraev <sirlibloger37@gmail.com>
2021-11-02 18:22:40 +0500

    New Js Site From Xurshidjon

---
+++ js/main.js
@@ -0,0 +1,39 @@
+var firstName = prompt('Ismingiz');
+alert(firstName);
+console.log(firstName);
+
+var firstSurname = prompt('Familyangiz');
+alert(firstSurname);
+console.log(firstSurname);
+
+var whereDoYouStudy = prompt('Qayerda Oqiysiz');
+alert(whereDoYouStudy);
+console.log(whereDoYouStudy);
+
+var whereDoYouWork = prompt('Qayerda ishlaysiz');
+alert(whereDoYouWork);
+console.log(whereDoYouWork);
+
+var whatAreYouDoingNow = prompt('Hozir nima qilyapsiz');
+alert(whatAreYouDoingNow);
+console.log(whatAreYouDoingNow);
+
+var whoAreYouWith = prompt('Kim bilan siz');
+alert(whoAreYouWith);
+console.log(whoAreYouWith);
+
+var whatTimeIsIt = prompt('Soat nechi boldi');
+alert(whatTimeIsIt);
+console.log(whatTimeIsIt);
+
+var whatIsTheNameOfTheManOnYourLeft = prompt('Chap tarafingizdagi odamni ismi nima');
+alert(whatIsTheNameOfTheManOnYourLeft);
+console.log(whatIsTheNameOfTheManOnYourLeft);
+
+var whatIsTheNameOfTheManOnYourRight = prompt('Ong tarafingizdagi odamni ismi nima');
+alert(whatIsTheNameOfTheManOnYourRight);
+console.log(whatIsTheNameOfTheManOnYourRight);
+
+var whichSchoolDoYouGoTo = prompt('qaysi maktabda oqiysiz');
+alert(whichSchoolDoYouGoTo);
+console.log(whichSchoolDoYouGoTo);
